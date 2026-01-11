6d455dffb160c0bab71d7ddc0fc6169d2208fa1b
Chiderah <chiderahonwumelu@gmail.com>
2026-01-11 15:26:57 +0000

    Fix TypeScript module error in config

---
+++ app/config.ts
@@ -0,0 +1,7 @@
+export const LAZORKIT_CONFIG = {
+  rpcUrl: 'https://api.devnet.solana.com',
+  portalUrl: 'https://portal.lazor.sh',
+  paymasterConfig: {
+    paymasterUrl: 'https://kora.devnet.lazorkit.com',
+  },
+};
